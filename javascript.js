26a401642b6fbe9b626d72ce5611eef3a1c5bcdd
Abhishek <as9044192702@gmail.com>
2024-11-13 23:22:05 +0530

    song name edit

--- javascript.js
+++ javascript.js
@@ -25,38 +25,38 @@ async function main() {
 
 }
 
-const playMusic = (track) => {
-  const trackPath = `/songs/${track}`;
+// const playMusic = (track) => {
+//   const trackPath = `/songs/${track}`;
 
-  if (currentlyPlaying === track) {
-    // If the same song is clicked, toggle play/pause
-    if (currentSong.paused) {
-      currentSong.play();
-      document.getElementsByClassName("playbutton")[0].innerHTML = `<img width="35" id="play" src="home.svg" alt="">`
-    } else {
-      currentSong.pause();
-       document.getElementsByClassName("playbutton")[0].innerHTML = originalplaybutton;
-    }
-  } else {
-    // If a different song is clicked, load and play the new song
-    currentSong.src = trackPath;
-    currentSong.play();
-    currentlyPlaying = track;
-  }
+//   if (currentlyPlaying === track) {
+//     // If the same song is clicked, toggle play/pause
+//     if (currentSong.paused) {
+//       currentSong.play();
+//       document.getElementsByClassName("playbutton")[0].innerHTML = `<img width="35" id="play" src="home.svg" alt="">`
+//     } else {
+//       currentSong.pause();
+//        document.getElementsByClassName("playbutton")[0].innerHTML = originalplaybutton;
+//     }
+//   } else {
+//     // If a different song is clicked, load and play the new song
+//     currentSong.src = trackPath;
+//     currentSong.play();
+//     currentlyPlaying = track;
+//   }
   
-  currentSong.onended = () => {
-    // Reset when the song ends
-    currentlyPlaying = null;
-  };
-};
+//   currentSong.onended = () => {
+//     // Reset when the song ends
+//     currentlyPlaying = null;
+//   };
+// };
 
-window.onload = function() {
+// window.onload = function() {
 
-  let originalplaybutton = '';
+//   let originalplaybutton = '';
   
-  const playButtonElement = document.getElementsByClassName("playbutton")[0];
-  originalplaybutton = playButtonElement.innerHTML;
-  console.log(originalplaybutton);
+//   const playButtonElement = document.getElementsByClassName("playbutton")[0];
+//   originalplaybutton = playButtonElement.innerHTML;
+//   console.log(originalplaybutton);
   main();
   console.log("hellohow are youy")
 
